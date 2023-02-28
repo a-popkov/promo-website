edf0daef4f07d0c554fed3d03c34359c32f9d3da
Andrey Popkov <popkov.info@gmail.com>
2023-02-28 23:39:34 +0300

    Add Hero section

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,27 @@
 <template>
+  <header>
 
+  </header>
   <main>
-    разметка
+    <section class="relative h-[72.5rem]">
+      <img class="absolute top-0 left-0 w-full h-[82.4375rem]" src="/src/assets/images/hero-bg.png" alt="">
+      <div class="px-14">
+        <div class="pt-32 pl-8">
+          <h1 class="mb-6 drop-shadow-3xl w-1/2 font-serif text-12xl uppercase text-white">Kate kulish</h1>
+          <ul role="list" class="relative z-10 flex gap-10 items-center pl-5 ml-3 text-[1.625rem] text-white marker:text-white marker:text-xl list-disc">
+            <li>Pureness</li>
+            <li>Energy</li>
+            <li>Practicing mindfulness</li>
+          </ul>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div class="mb-12 px-14">
+        <h2 class="font-serif text-11xl uppercase text-black">Gallery</h2>
+      </div>
+
+    </section>
   </main>
 
 </template>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,42 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Poppins', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', '"Helvetica Neue"', '"Noto Sans"', 'sans-serif'],
+        'serif': ['Noto Serif Display', 'Georgia',' ui-serif', 'Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
+      },
+      fontSize: {
+        '10xl': ['11.25rem', {
+          lineHeight: '1.22',
+          fontWeight: '400',
+        }],
+        '11xl': ['12.5rem', {
+          lineHeight: '1.1',
+          fontWeight: '400',
+        }],
+        '12xl': ['15.625rem', {
+            lineHeight: '.88',
+            fontWeight: '400',
+        }],
+      },
+      dropShadow: {
+        '3xl': [
+          '1px 1px 0 rgba(0, 0, 0, .8)',
+          '-.1px -.1px 0 rgba(0, 0, 0, 1)',
+          '2px -1px 0 rgba(0, 0, 0, .8)',
+          '0 3px 3px rgba(0, 0, 0, .8)',
+          '0 6px 20px rgba(0, 0, 0, 0.7)',
+        ]
+      }
+    },
   },
   plugins: [],
 }
+
+          // '1px 1px 0 rgba(0, 0, 0, .9)',
+          // '-1px -1px 0 rgba(0, 0, 0, .9)',
+          // '-1px -1px 0 rgba(0, 0, 0, .9)',
+          // '1px -1px 0 rgba(0, 0, 0, .9)',
+          // '0 3px 3px rgba(0, 0, 0, .8)',
+          // '0 6px 15px rgba(0, 0, 0, 0.6)',
